3459c5552252fc42ebf2d6c1bc9150bf37abc46d
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-04-08 08:43:06 +0300

    Added possibility to update task by employee

--- src/main/java/org/task_manager/simpletaskmanager/controller/employee/EmployeeController.java
+++ src/main/java/org/task_manager/simpletaskmanager/controller/employee/EmployeeController.java
@@ -1,11 +1,9 @@
 package org.task_manager.simpletaskmanager.controller.employee;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.task_manager.simpletaskmanager.dto.TaskDto;
 import org.task_manager.simpletaskmanager.service.employee.EmployeeService;
 
@@ -21,5 +19,12 @@ public class EmployeeController {
     @GetMapping("/tasks")
     public ResponseEntity<List<TaskDto>> getTasksByUserId() {
         return ResponseEntity.ok(employeeService.getTasksByUserId());
+    }
+
+    @GetMapping("/task/{id}/{status}")
+    public ResponseEntity<TaskDto> updateTaskStatus(@PathVariable Long id, @PathVariable String status) {
+        TaskDto taskDto = employeeService.updateTaskStatus(id, status);
+        if(taskDto == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.ok(taskDto);
     }
 }
--- src/main/java/org/task_manager/simpletaskmanager/service/employee/EmployeeServiceImpl.java
+++ src/main/java/org/task_manager/simpletaskmanager/service/employee/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.task_manager.simpletaskmanager.dto.TaskDto;
+import org.task_manager.simpletaskmanager.enums.TaskStatus;
 import org.task_manager.simpletaskmanager.model.Task;
 import org.task_manager.simpletaskmanager.model.User;
 import org.task_manager.simpletaskmanager.repositories.TaskRepository;
@@ -11,6 +12,7 @@ import org.task_manager.simpletaskmanager.utils.JwtUtil;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,5 +33,26 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         throw new EntityNotFoundException("User not found");
+    }
+
+    @Override
+    public TaskDto updateTaskStatus(Long id, String status) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            task.setTaskStatus(mapStringToTaskStatus(status));
+            return taskRepository.save(task).getTaskDto();
+        }
+        throw new EntityNotFoundException("Task not found");
+    }
+
+    private TaskStatus mapStringToTaskStatus(String status) {
+        return switch (status) {
+            case "PENDING" -> TaskStatus.PENDING;
+            case "IN_PROGRESS" -> TaskStatus.IN_PROGRESS;
+            case "COMPLETED" -> TaskStatus.COMPLETED;
+            case "DEFERRED" -> TaskStatus.DEFERRED;
+            default -> TaskStatus.CANCELLED;
+        };
     }
 }
